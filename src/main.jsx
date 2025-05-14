2d4e95e1c21e1f1470839390d0890368b58d43c9
Kaelan Brose <kbrose2776@gmail.com>
2025-05-14 08:47:51 -0500

    fixed app comonent

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './login.jsx'
+import Routing from './routing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routing />
     <Login />
   </StrictMode>,
 )
