ab75a0b51e9588f0e47a907add3858949f9862ae
Kaelan Brose <kbrose2776@gmail.com>
2025-05-14 08:27:07 -0500

    initial commit

---
+++ src/API_Scripts/redirect.js
@@ -0,0 +1,45 @@
+import CryptoJS from 'crypto-js';
+
+export async function redirectToAuthCodeFlow(clientId) {
+  console.log('hello')
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://192.168.0.35:3000/dashboard");
+  params.append("scope", "user-read-private user-read-email");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+  // Create a SHA-256 hash of the code verifier
+  const hash = CryptoJS.SHA256(codeVerifier);
+
+  // Convert hash to a Base64 URL encoding
+  const base64 = hash.toString(CryptoJS.enc.Base64);
+  return base64UrlEncode(base64);
+}
+
+function base64UrlEncode(base64) {
+  return base64
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, ''); // Remove padding
+}
---
+++ src/login.jsx
@@ -0,0 +1,15 @@
+import { redirectToAuthCodeFlow } from "./API_Scripts/redirect.js";
+
+const clientId = 'c7d7db2ffd7e4d229d6c8977e5792dee';
+
+function Login() {
+
+  return (
+    <>
+      <h1>Login with Spotify API</h1>
+      <button onClick={() => redirectToAuthCodeFlow(clientId)}>Login</button>
+    </>
+  );
+}
+
+export default Login;
