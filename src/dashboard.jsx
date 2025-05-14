e9e51cdc6e009a9db49e66d68007536223a4ebd0
Kaelan Brose <kbrose2776@gmail.com>
2025-05-14 09:17:58 -0500

    refacured dashboard

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,31 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getAccessToken } from "./API_Scripts/getAccess";
 import { fetchProfile } from "./API_Scripts/getProfile";
 
-
 const clientId = 'c7d7db2ffd7e4d229d6c8977e5792dee';
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
-const accessToken = getAccessToken(clientId, code);
-let profile = fetchProfile(accessToken);
 
 function Dashboard() {
+  const [profile, setProfile] = useState(null);
   const location = useLocation();
 
   useEffect(() => {
-    if (code) {
-      console.log("Spotify authorization code:", code);
-      // You can exchange the code for a token, make API calls, etc.
+    const params = new URLSearchParams(location.search);
+    const code = params.get("code");
+
+    async function init() {
+      if (code) {
+        console.log("Spotify authorization code:", code);
+        const accessToken = await getAccessToken(clientId, code);
+        const fetchedProfile = await fetchProfile(accessToken);
+        setProfile(fetchedProfile);
+      }
     }
-  }, [code]);
+
+    init();
+  }, [location.search]);
+
+  if (!profile) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <h1>Hello</h1>
       <h1>{profile.username}</h1>
       <p>{profile.email}</p>
-      <img src={profile.images[0].url} alt="" />
+      <img src={profile.images?.[0]?.url} alt="Profile" />
       <p>{profile.product}</p>
     </>
   );
