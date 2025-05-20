81adb79c6d483d7698837a87abd3cc10925f27b0
Kaelan Brose <kbrose2776@gmail.com>
2025-05-20 10:13:54 -0500

    fix?

--- src/Components/Song.jsx
+++ src/Components/Song.jsx
@@ -5,7 +5,7 @@ function Song(props) {
   return (
     <div className='container'>
       <div onClick='' >
-        <img src={track['album']['images'][0].url} alt="" className='cover'/>
+        <img src={props.topTracks['album']['images'][0].url} alt="" className='cover'/>
         <div className='names'>
           <p className='song'>Song</p>
           <p className="artist">Artist</p>
