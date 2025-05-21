c00239f5cc72a4d605ce00e90c4b03ebd6e735cb
Kaelan Brose <kbrose2776@gmail.com>
2025-05-21 10:08:35 -0500

    with all

---
+++ src/API_Scripts/getRecentPlays.js
@@ -0,0 +1,7 @@
+export async function getRecentPlays(token) {
+  const result = await fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=6`, {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+
+  return await result.json();
+}
