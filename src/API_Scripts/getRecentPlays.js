e5468666af1ba8090fcd87a007d68c6f55ed29e1
Kaelan Brose <kbrose2776@gmail.com>
2025-05-21 10:47:45 -0500

    changed recent limit

--- src/API_Scripts/getRecentPlays.js
+++ src/API_Scripts/getRecentPlays.js
@@ -1,5 +1,5 @@
 export async function getRecentPlays(token) {
-  const result = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=6', {
+  const result = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=8', {
     method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
 
