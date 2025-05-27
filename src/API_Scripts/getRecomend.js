f42499c9296da82b8f31b6716819fbfa6647bcc4
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 20:25:11 -0500

    changed

--- src/API_Scripts/getRecomend.js
+++ src/API_Scripts/getRecomend.js
@@ -2,15 +2,16 @@ export async function getRecomend(tracks) {
   // pass in tracks?.items
   const apiKey = '65bc1d83884f288438e63b1ab3980e07';
   console.log(tracks)
-  let returnArr = [];
 
-  if (!tracks){ console.error('No top tracks'); return; }
+  if (!tracks) { console.error('No top tracks'); return; }
 
-  tracks.slice(0, 5).map(track => {
-    const result = fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(track?.artist?.name)}&track=${encodeURIComponent(track?.name)}&api_key=${api}&format=json`)
+  const fetchTracks = tracks.slice(0, 5).forEach(async (track) => {
+    const result = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(track?.artist?.name)}&track=${encodeURIComponent(track?.name)}&api_key=${apiKey}&format=json`)
 
-    returnArr.push(result.json())
-  })  
+    const data = await result.json();
+    return data;
+  })
 
-  return returnArr;
+  const returnArr = await Promise.all(fetchPromises);
+  return returnArr.filter(Boolean);
 }
