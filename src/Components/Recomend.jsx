529b2832d0f2e4f41f64e82b66d2de70dcedacc8
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 21:02:59 -0500

    changed

--- src/Components/Recomend.jsx
+++ src/Components/Recomend.jsx
@@ -2,11 +2,11 @@ import './Recomend.css'
 
 import Song from './Song'
 
-function Recomend(props) {
-  console.log(`recomend track: ${props.track}`)
+function Recomend({ track }) {
+  console.log(`recomend track: ${track}`)
   return (
     <div className='recomendContainer'>
-      <Song track={props.track} />
+      <Song track={track} />
       <div className='buttonContainer'>
         <button className='recomendButton'>
           <img src="/plus.svg" alt="" className='recomendButton'/>
