1c902ab1630ad393867a9c91287679349eaed85f
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 21:37:32 -0500

    test

--- src/API_Scripts/search.js
+++ src/API_Scripts/search.js
@@ -1,7 +1,10 @@
 export async function search(token, qString, type){
+  console.log(token, encodeURIComponent(qString), type)
   const result = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(qString)}&type=${type}`, {
     method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
   
-  return await result.json();
+  const data = await result.json();
+  console.log(data)
+  return data;
 }
