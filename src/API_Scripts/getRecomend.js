1dc72d3d1b95c9270d2fc500e4d95f6937a5f2f7
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 20:39:50 -0500

    test

--- src/API_Scripts/getRecomend.js
+++ src/API_Scripts/getRecomend.js
@@ -11,6 +11,9 @@ export async function getRecomend(tracks) {
     const artist = track.artist?.[0]?.name
     const trackName = track.name
 
+    console.log(`track : ${trackName}, artist: ${artist}`)
+    console.log(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(trackName)}&api_key=${apiKey}&format=json`)
+
     const result = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(trackName)}&api_key=${apiKey}&format=json`)
 
     const data = await result.json();
