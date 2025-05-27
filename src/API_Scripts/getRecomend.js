4302f4be2dc6dc73cc5c15659d2159fc099ac0c0
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 20:16:51 -0500

    added recomend heler function

---
+++ src/API_Scripts/getRecomend.js
@@ -0,0 +1,15 @@
+export async function getRecomend(tracks) {
+  // pass in tracks?.items
+  const apiKey = '65bc1d83884f288438e63b1ab3980e07';
+  let returnArr = [];
+
+  if (!tracks){ console.error('No top tracks'); return; }
+
+  tracks.split(0, 5).map(track => {
+    const result = fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(track?.artist?.name)}&track=${encodeURIComponent(track?.name)}&api_key=${api}&format=json`)
+
+    returnArr.push(result.json())
+  })  
+
+  return returnArr;
+}
