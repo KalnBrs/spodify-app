357be8eb966d7abb92fba9758eefa5153a7494e6
Kaelan Brose <kbrose2776@gmail.com>
2025-05-26 20:58:55 -0500

    test

--- src/API_Scripts/getRecomend.js
+++ src/API_Scripts/getRecomend.js
@@ -3,20 +3,12 @@ const clean = str => str?.replace(/\(.*?\)|\[.*?\]|-.*$|feat\..*$/gi, '').trim()
 export async function getRecomend(tracks) {
   // pass in tracks?.items
   const apiKey = '65bc1d83884f288438e63b1ab3980e07';
-  console.log(tracks)
-  console.log("typeof tracks:", typeof tracks);
 
   if (!tracks) { console.error('No top tracks'); return; }
 
   const fetchTracks = tracks.map(async (track) => {
-    console.log(track)
-
-
     const artist = clean(track.artists?.[0]?.name)
     const trackName = clean(track.name)
-
-    console.log(`track : ${trackName}, artist: ${artist}`)
-    console.log(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(trackName)}&api_key=${apiKey}&format=json`)
 
     const result = await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(trackName)}&api_key=${apiKey}&format=json`)
 
@@ -24,7 +16,6 @@ export async function getRecomend(tracks) {
     if ( Array.isArray(data?.similartracks?.track) && data.similartracks.track.length > 0) {
       return data; 
     } else {
-      console.info(`No similar tracks for: ${artist} - ${trackName}`);
       return null;
     }
   })
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -157,6 +157,7 @@ function Dashboard() {
         <div className="card" id='recomend'>
           <h1 className="header">Recomended Songs</h1>
           {recomend?.slice(0, 5).map((track) => {
+            console.log(track?.similartracks?.track?.[0]);
             <Recomend track={track?.similartracks?.track?.[0]} />
           })}
         </div>
